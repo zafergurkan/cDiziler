244cfda13cf7df0cb47f4a5ee1406bb0f75df52a
Zafer Gurkan <zafergurkan@outlook.com>
2017-09-25 11:10:44 +0300

    açıklama satırları eklendi

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+
+	//iki sütun ve satırdan oluşan çok boyutlu bir dizi tanımlandı.
 	int dizi[2][2];
 
+	//dizilere elaman ataması yapıldı.
 	dizi[0][0]=10;
 	dizi[0][1]=20;
 	dizi[1][0]=30;
 	dizi[1][1]=40;
 
+	
+	//Diziler ekrana yazdırıldı.
 	printf("%d",dizi[0][0]);
 	printf("   ");
 	printf("%d\n",dizi[0][1]);
