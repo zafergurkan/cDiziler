6a6217739dd1bc444227e832324f725fe36f8a1a
Zafer Gurkan <zafergurkan@outlook.com>
2017-09-25 11:05:09 +0300

    "Dizi oluşturuldu"

--- main.c
+++ main.c
@@ -4,5 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+	int dizi[2][2];
+
+	dizi[0][0]=10;
+	dizi[0][1]=20;
+	dizi[1][0]=30;
+	dizi[1][1]=40;
+	
 	return 0;
 }
